3ebab9fbf6f0f5292c8e303db4d3a3a974808c65
Selga Muiznieks <selga.muiznieks@gmail.com>
2021-09-28 12:35:18 -0400

    add bootstrap

--- week17/src/index.js
+++ week17/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// npm install bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import App from './components/App';
 
 ReactDOM.render(
