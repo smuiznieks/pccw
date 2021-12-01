acf6efb9ebaf6a6c7d04ae45bb281e746ea08211
Selga Muiznieks <selga.muiznieks@gmail.com>
2021-12-01 17:47:34 -0500

    starter auth.js file and instructions

--- week24/README.md
+++ week24/README.md
@@ -1,3 +1,62 @@
 # Week 24
 
+1. `npm install` just in case! and `npm i jsonwebtoken`
+2. In server.js:
+`import jwt from 'jsonwebtoken';`
+`const accessTokenSecret = 'somerandomaccesstoken';`
+```
+// Copying this function from the starter files
+const authenticateJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  console.log(`Headers: ${JSON.stringify(req.headers)}`);
+  console.log(`Body: ${JSON.stringify(req.body)}`);
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
 
+    jwt.verify(token, accessTokenSecret, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+
+      req.user = user;
+      next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
+}
+```
+3. Test endpoint in Postman
+4. Add to middleware: `app.use(selgaMiddleware, authenticateJWT);`
+5. Restart server, test endpoint again
+6. Login by posting to the login endpoint: `http://localhost:4000/login` with the following body:
+```
+{
+  "username": "john",
+  "password": "password123admin"
+}
+```
+7. Test endpoint again
+8. Add logic to check authorization for deleting:
+```
+// DELETE
+app.delete('/delete/:id', (req, res) => {
+  const { role } = req.user;
+
+  if (role != 'admin') {
+    return res.sendStatus(403);
+  }
+
+  // ... more code
+
+});
+```
+9. Login by posting to the login endpoint: `http://localhost:4000/login` with the following body:
+```
+{
+  "username": "anna",
+  "password": "password123member"
+}
+```
+10. Attempt to delete an entry - forbidden
+11. Re-attempt with admin user - success
---
+++ week24/auth.js
@@ -0,0 +1,88 @@
+// This is copied from the starter files with two changes (update imports and change to port 4000)
+// Also I fixed typos on lines 25 and 79
+
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+const accessTokenSecret = 'somerandomaccesstoken';
+const refreshTokenSecret = 'somerandomstringforrefreshtoken';
+
+const users = [
+    {
+        username: 'john',
+        password: 'password123admin',
+        role: 'admin'
+    }, {
+        username: 'anna',
+        password: 'password123member',
+        role: 'member'
+    }
+]
+
+// TYPO!!!!!!!
+var refreshTokens = [];
+
+app.use(bodyParser.json());
+
+app.post('/login', (req, res) => {
+    // read username and password from request body
+    const { username, password } = req.body;
+
+    // filter user from the users array by username and password
+    const user = users.find(u => { return u.username === username && u.password === password });
+
+    if (user) {
+        // generate an access token
+        const accessToken = jwt.sign({ username: user.username, role: user.role }, accessTokenSecret, { expiresIn: '20m' });
+        const refreshToken = jwt.sign({ username: user.username, role: user.role }, refreshTokenSecret);
+
+        refreshTokens.push(refreshToken);
+
+        res.json({
+            accessToken,
+            refreshToken
+        });
+    } else {
+        res.send('Username or password incorrect');
+    }
+});
+
+app.post('/token', (req, res) => {
+    const { token } = req.body;
+
+    if (!token) {
+        return res.sendStatus(401);
+    }
+
+    if (!refreshTokens.includes(token)) {
+        return res.sendStatus(403);
+    }
+
+    jwt.verify(token, refreshTokenSecret, (err, user) => {
+        if (err) {
+            return res.sendStatus(403);
+        }
+
+        const accessToken = jwt.sign({ username: user.username, role: user.role }, accessTokenSecret, { expiresIn: '20m' });
+
+        res.json({
+            accessToken
+        });
+    });
+});
+
+app.post('/logout', (req, res) => {
+    const { token } = req.body;
+    // TYPO!!!!!!!
+    refreshTokens = refreshTokens.filter(t => t !== token);
+
+    res.send("Logout successful");
+});
+
+// Change the port to 4000!
+app.listen(4000, () => {
+    console.log('Authentication service started on port 4000');
+});
